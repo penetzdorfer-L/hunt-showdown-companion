1dfcdb625b6d39db859711f1b68dcfaeed855406
penetzdorfer-L <lukas.penetzdorfer@gmail.com>
2023-05-31 12:12:49 +0200

    initialized project and add gitignores

---
+++ server-huntcompanion/src/main/kotlin/com/lupe/huntcompanion/HuntcompanionApplication.kt
@@ -0,0 +1,11 @@
+package com.lupe.huntcompanion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HuntcompanionApplication
+
+fun main(args: Array<String>) {
+	runApplication<HuntcompanionApplication>(*args)
+}
---
+++ server-huntcompanion/src/test/kotlin/com/lupe/huntcompanion/HuntcompanionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lupe.huntcompanion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HuntcompanionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
