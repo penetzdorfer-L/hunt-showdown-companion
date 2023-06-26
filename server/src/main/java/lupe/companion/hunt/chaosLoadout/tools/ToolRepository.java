6659d582516a6dbc00b52360ab5aef2f35bf6c82
penetzdorfer-L <lukas.penetzdorfer@gmail.com>
2023-06-26 20:45:47 +0200

    finished tool randomization

--- server/src/main/java/lupe/companion/hunt/chaosLoadout/tools/ToolRepository.java
+++ server/src/main/java/lupe/companion/hunt/chaosLoadout/tools/ToolRepository.java
@@ -3,7 +3,11 @@ package lupe.companion.hunt.chaosLoadout.tools;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ToolRepository extends JpaRepository<Tool, Long> {
     Tool findToolsByNameContainingIgnoreCase(String parameter);
+    List<Tool> findToolsByTypeIsIgnoreCase(String parameter);
+    Tool findToolsByToolID(String tool);
 }
