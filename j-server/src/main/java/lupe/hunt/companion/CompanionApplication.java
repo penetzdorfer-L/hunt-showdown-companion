542212ed38843b408be9925aa4024476ae89f14e
penetzdorfer-L <lukas.penetzdorfer@gmail.com>
2023-07-03 11:43:13 +0200

    init development branch

---
+++ j-server/src/main/java/lupe/hunt/companion/CompanionApplication.java
@@ -0,0 +1,13 @@
+package lupe.hunt.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionApplication.class, args);
+	}
+
+}
