bd58f2de5c2dc8141886174e45aec906f5690e2b
penetzdorfer-L <lukas.penetzdorfer@gmail.com>
2023-07-03 12:05:54 +0200

    renamed to companion application

--- j-server/src/main/java/lupe/companion/hunt/CompanionApplication.java
+++ j-server/src/main/java/lupe/companion/hunt/CompanionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HuntApplication {
+public class CompanionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HuntApplication.class, args);
+		SpringApplication.run(CompanionApplication.class, args);
 	}
 
 }
